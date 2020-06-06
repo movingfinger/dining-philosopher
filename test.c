ff2f4f2f74b4d4581664187a2fb79be203567362
Sanghyun Ko <movingfinger7@gmail.com>
2020-06-06 23:43:37 +0900

    update 2

--- test.c
+++ test.c
@@ -15,10 +15,8 @@
 int main(int ac, char **av)
 {
 	int i;
+	long time_basic;
 
-	sleep_seconds = 0;
-	num_philo = 5;
-	food_limit = 5;
 	if (ac == 4)
 	{
 		num_philo = atoi(av[1]);
@@ -27,54 +25,21 @@ int main(int ac, char **av)
 	}
 	num_can_eat = num_philo - 1;
 	food_temp = food_limit;
+	time_basic = 0;
 
-	chopstick = (pthread_mutex_t *)malloc(sizeof(pthread_mutex_t) * num_philo);
+	m_fork = (pthread_mutex_t *)malloc(sizeof(pthread_mutex_t) * num_philo);
 	philo = (pthread_t *)malloc(sizeof(pthread_t) * num_philo);
 	pthread_mutex_init (&food_lock, NULL);
 	pthread_mutex_init (&num_can_eat_lock, NULL);
-	for (i = 0; i < num_philo; i++)
-		pthread_mutex_init (&chopstick[i], NULL);
-	for (i = 0; i < num_philo; i++)
-		pthread_create (&philo[i], NULL, philosopher, (void *)i);
 
-
+	for (i = 0; i < num_philo; i++)
+	{
+		pthread_mutex_init (&m_fork[i], NULL);
+		Spawn(&time_basic, &m_fork, i);
+	}
 	for (i = 0; i < num_philo; i++)
 		pthread_join (philo[i], NULL);
 	return (0);
-}
-
-void *philosopher (void *num)
-{
-	int id;
-	int i;
-	int left_chopstick;
-	int right_chopstick;
-	int f;
-
-	id = (int)num;
-	printf ("Philosopher %d is done thinking and now ready to eat. \n", id);
-	right_chopstick = id;
-	left_chopstick = id + 1;
-
-	// Wrap around the forks.
-	if (left_chopstick == num_philo)
-		left_chopstick = 0;
-
-		while (f = food_on_table())
-		{
-			get_token ();
-
-			grab_chopstick (id, right_chopstick, "right");
-			grab_chopstick (id, left_chopstick, "left");
-
-			printf ("Philosopher %d: eating\n", id);
-			usleep (DELAY * (food_temp - f + 1));
-			down_chopsticks (left_chopstick, right_chopstick);
-			return_token ();
-		}
-
-		printf ("Philosopher %d is done eating. \n", id);
-		return (NULL);
 }
 
 int food_on_table ()
@@ -89,16 +54,146 @@ int food_on_table ()
 	return (myfood);
 }
 
-void grab_chopstick (int phil, int c, char *hand)
+void *philosopher (void *arg)
 {
-	pthread_mutex_lock (&chopstick[c]);
-	printf ("Philosopher %d: got %s chopstick %d\n", phil, hand, c);
+	t_philosophers *philo;
+	pthread_t thread;
+	int i;
+	int left_fork;
+	int right_fork;
+
+	philo = arg;
+	printf ("Philosopher %d is done thinking and now ready to eat. \n", philo->pos);
+	right_fork = philo->pos;
+	left_fork = philo->pos + 1;
+	i = 0;
+
+	// Wrap around the forks.
+	if (left_fork == num_philo)
+		left_fork = 0;
+	pthread_create(&thread, NULL, life_cycle, philo);
+	//while (i != food_limit)
+	while (i = food_on_table())
+	{
+		eat(philo, left_fork, right_fork);
+		pthread_create(&thread, NULL, life_cycle, philo);
+		//if (food_limit > 0)
+		//{
+		//	if (++i >= food_limit)
+		//	{
+		//		printf("food count fulfilled\n");
+		//		break;
+		//	}
+		//}
+		usleep(sleep_seconds * 1000);
+		philo->state = 1;
+	}
+
+	printf ("Philosopher %d is done eating. \n", philo->pos);
+
+	if (++*philo->death >= num_philo)
+	{
+		printf("All philosophers died!\n");
+		exit (0);
+	}
+	pthread_exit(0);
+	return (NULL);
 }
 
-void down_chopsticks (int c1, int c2)
+void Spawn(long *time, pthread_mutex_t **m_fork, int i)
 {
-	pthread_mutex_unlock (&chopstick[c1]);
-	pthread_mutex_unlock (&chopstick[c2]);
+	t_philosophers *philo;
+	pthread_t thread;
+	static int death;
+
+	philo = (t_philosophers *)malloc(sizeof(t_philosophers));
+	philo->pos = i;
+	philo->eat_count = food_limit;
+	philo->state = 1; //thinking
+	philo->death = &death;
+	philo->time = time;
+
+	pthread_create(&thread, NULL, philosopher, philo);
+}
+
+void eat(t_philosophers *philo, int left, int right)
+{
+	int id;
+
+	id = philo->pos;
+	philo->state = 2;
+	get_token ();
+
+	grab_fork (id, right, "right");
+	grab_fork (id, left, "left");
+
+	printf ("Philosopher %d: eating\n", id);
+	usleep (sleep_seconds * 1000);
+	down_forks (left, right);
+	return_token ();
+}
+
+//void eat(int id, int left, int right)
+//{
+//	get_token ();
+
+//	grab_fork (id, right, "right");
+//	grab_fork (id, left, "left");
+
+//	printf ("Philosopher %d: eating\n", id);
+//	usleep (sleep_seconds * 1000);
+//	down_forks (left, right);
+//	return_token ();
+//}
+
+//void *philosopher (void *num)
+//{
+//	int id;
+//	int i;
+//	int left_fork;
+//	int right_fork;
+//	int f;
+
+//	id = (int)num;
+//	printf ("Philosopher %d is done thinking and now ready to eat. \n", id);
+//	right_fork = id;
+//	left_fork = id + 1;
+
+//	// Wrap around the forks.
+//	if (left_fork == num_philo)
+//		left_fork = 0;
+
+//	while (f = food_on_table())
+//	{
+//		eat(id, left_fork, right_fork);
+//	}
+
+//	printf ("Philosopher %d is done eating. \n", id);
+//	return (NULL);
+//}
+
+//int food_on_table ()
+//{
+//	int myfood;
+
+//	pthread_mutex_lock (&food_lock);
+//	if (food_limit > 0)
+//		food_limit--;
+//	myfood = food_limit;
+//	pthread_mutex_unlock (&food_lock);
+//	return (myfood);
+//}
+
+void grab_fork (int phil, int c, char *hand)
+{
+	pthread_mutex_lock (&m_fork[c]);
+	printf ("Philosopher %d: has taken %s fork %d\n", phil, hand, c);
+}
+
+void down_forks (int c1, int c2)
+{
+	pthread_mutex_unlock (&m_fork[c1]);
+	pthread_mutex_unlock (&m_fork[c2]);
 }
 
 int get_token ()
@@ -125,4 +220,48 @@ void return_token ()
 	pthread_mutex_lock (&num_can_eat_lock);
 	num_can_eat++;
 	pthread_mutex_unlock (&num_can_eat_lock);
+}
+
+void *time_ct(void *var)
+{
+	struct timeval time;
+	struct timeval time2;
+	long *elapsed;
+
+	elapsed = var;
+	gettimeofday(&time, NULL);
+	while (1)
+	{
+		gettimeofday(&time2, NULL);
+		*elapsed = (((time2.tv_sec - time.tv_sec) * 1000 + ((time2.tv_usec - time.tv_usec) / 1000)));
+		if (*elapsed < 0)
+			*elapsed *= -1;
+	}
+}
+
+void *life_cycle(void *arg)
+{
+	t_philosophers *philo;
+	struct timeval time;
+	struct timeval time2;
+	long long elapsed;
+
+	philo = arg;
+	gettimeofday(&time, NULL);
+	elapsed = 0;
+	while (1)
+	{
+		gettimeofday(&time2, NULL);
+		elapsed = (((time2.tv_sec - time.tv_sec) * 1000 + ((time2.tv_usec - time.tv_usec) / 1000)));
+		// if timestamp over time to die, then die.
+		if (die_seconds <= elapsed)
+		{
+			philo->current = *philo->time;
+			philo->state = 3;
+			printf("Philosopher %d dies\n", philo->pos);
+			exit (0);
+		}
+		if (philo->state == 0)	// if eating state
+			pthread_exit(NULL);
+	}
 }
--- test.h
+++ test.h
@@ -23,23 +23,49 @@
 
 #define DELAY 5000
 
-pthread_mutex_t *chopstick;
-pthread_t *philo;
+pthread_mutex_t *m_fork;			// mutex for fork
+pthread_t *philo;					
 pthread_mutex_t food_lock;
-pthread_mutex_t num_can_eat_lock;
+pthread_mutex_t num_can_eat_lock;	
 
-long long num_philo;
-long long num_can_eat;
-int sleep_seconds;
-int food_limit;
-int food_temp;
+long long num_philo = 4;			// number of philosophers
+long long num_can_eat;				// num_philo - 1 for token system
+int sleep_seconds = 5;				// time to sleep
+int think_seconds = 5;				// time to sleep
+int eat_seconds = 5;				// time to eat
+int die_seconds = 5;				// time to die
+int food_limit = 5;					// number of times each philosopher must eat. If -1, it means not defined.
+int food_temp;						// counter for food left.
 
-void *philosopher (void *id);
-void grab_chopstick (int, int, char *);
-void down_chopsticks (int, int);
+/*
+** state 0: eating
+** state 1: thinking
+** state 2: sleeping
+** state 3: died
+*/
+
+typedef struct	s_philosophers
+{
+	int			pos;
+	int			eat_count;
+	int			state;
+	long		current;
+	long		*time;
+	int			*death;
+}				t_philosophers;
+
+//void *philosopher (void *id);
+void *philosopher (void *arg);
+void grab_fork (int, int, char *);
+void down_forks (int, int);
 
 int food_on_table ();
 int get_token ();
 void return_token ();
+//void eat(int id, int left, int right);
+void eat(t_philosophers *philo, int left, int right);
+void *life_cycle(void *arg);
+void *time_ct(void *var);
+void Spawn(long *time, pthread_mutex_t **m_fork, int i);
 
 #endif
